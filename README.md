85cf6cafbdf8c20fd7619719f6e92b9af344a1b1
kevenfz <keven.ferreira@hotmail.com>
2022-03-18 20:00:25 -0300

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Projeto_Blue-ProjetoMVC
+# Projeto Pokemon - Semana 2 - BlueEdTech
+
+## Descrição do Projeto
+Este projeto foi feito com fins de aprendizagem no cruso de FullStack (Enfase backend).
+
+## Stacks
+Javascript - <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+HTML - <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" />
+CSS - <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />
+Express - <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
+Node.JS - <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+
+## Personal
+Keven Ferreira Moraes
+<img src="https://media-exp1.licdn.com/dms/image/D5603AQEGAyEFCm3PbA/profile-displayphoto-shrink_800_800/0/1644422955556?e=1652918400&v=beta&t=nB4iD4YVwQzvtuHFHVpc27-vcXalkd0C6UV7DkE7EUo" />
+<a>https://www.linkedin.com/in/keven-ferreira-moraes-4327a463/</a>
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const req = require("express/lib/request");
 const app = express();
 const port = 3003;
 const path = require("path");
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
 
 const pokemons = [
     {
@@ -28,36 +30,39 @@ const pokemons = [
         tipo: "Type: Water",
         img: "https://i.imgur.com/xONwm7L.gif",
     },
-    {
-        id: "Nº: 04",
-        nome: "Jigglypuff" ,
-        descricao: "Jigglypuff has top-notch lung capacity, even by comparison to other Pokémon. It won’t stop singing its lullabies until its foes fall asleep." ,
-        tipo: "Type: Fairy",
-        img: "https://i.pinimg.com/originals/e0/db/72/e0db72594143348df0ba94a98212cb13.gif",
-    },
-    {
-        id: "Nº: 05",
-        nome: "Diglett" ,
-        descricao: "If a Diglett digs through a field, it leaves the soil perfectly tilled and ideal for planting crops." ,
-        tipo: "Type: Ground",
-        img: "https://i.pinimg.com/originals/94/6f/61/946f6177a8c56ac8910c3f26e1c729bc.gif",
-    },
-    {
-        id: "Nº: 06",
-        nome: "Slowpoke " ,
-        descricao: "Slow-witted and oblivious, this Pokémon won’t feel any pain if its tail gets eaten. It won’t notice when its tail grows back, either." ,
-        tipo: "Type: Psychic",
-        img: "https://kingdomofzealblog.files.wordpress.com/2017/05/slowpoke-6.gif",
-    },
+//     {
+//         id: "Nº: 04",
+//         nome: "Jigglypuff" ,
+//         descricao: "Jigglypuff has top-notch lung capacity, even by comparison to other Pokémon. It won’t stop singing its lullabies until its foes fall asleep." ,
+//         tipo: "Type: Fairy",
+//         img: "https://i.pinimg.com/originals/e0/db/72/e0db72594143348df0ba94a98212cb13.gif",
+//     },
+//     {
+//         id: "Nº: 05",
+//         nome: "Diglett" ,
+//         descricao: "If a Diglett digs through a field, it leaves the soil perfectly tilled and ideal for planting crops." ,
+//         tipo: "Type: Ground",
+//         img: "https://i.pinimg.com/originals/94/6f/61/946f6177a8c56ac8910c3f26e1c729bc.gif",
+//     },
+//     {
+//         id: "Nº: 06",
+//         nome: "Slowpoke " ,
+//         descricao: "Slow-witted and oblivious, this Pokémon won’t feel any pain if its tail gets eaten. It won’t notice when its tail grows back, either." ,
+//         tipo: "Type: Psychic",
+//         img: "https://kingdomofzealblog.files.wordpress.com/2017/05/slowpoke-6.gif",
+//     },
 ]
 
 app.get("/", (req, res) => {
-    res.send("Hello world!")
-});
-
-app.get("/index", (req, res) => {
     res.render("index.ejs", {pokemons})
 });
+
+app.post("/add", (req, res) => {
+    const pokemon = req.body;
+    pokemons.push(pokemon)
+
+    res.redirect("/");
+})
 
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
 
