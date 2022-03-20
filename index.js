758e831f2e81110c1f6941f81bd85a129140af0e
kevenfz <keven.ferreira@hotmail.com>
2022-03-19 23:23:03 -0300

    Quase pronto

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ const pokemons = [
         nome: "Charmander" ,
         descricao: "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail." ,
         tipo: "Fire",
+        tamanho: "0.6 m",
+        peso: "8.5 kg",
+        genero: "Masculino - Feminino",
         img: "https://64.media.tumblr.com/349aff504a8ac16a58055d2b9d7cad34/0efac011cf014221-ea/s500x750/e8c8c877aeff8f51ca54fa87afeff6b424242660.gifv",
     },
     {
@@ -21,6 +24,9 @@ const pokemons = [
         nome: "Pikachu" ,
         descricao: "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy." ,
         tipo: "Electric",
+        tamanho: "0.4 m",
+        peso: "6.0 kg",
+        genero: "Masculino - Feminino",
         img: "https://i.gifer.com/OqDo.gif",
     },
     {
@@ -28,6 +34,9 @@ const pokemons = [
         nome: "Squirtle" ,
         descricao: "When it retracts its long neck into its shell, it squirts out water with vigorous force." ,
         tipo: "Water",
+        tamanho: "0.5 m",
+        peso: "9.0 kg",
+        genero: "Masculino - Feminino",
         img: "https://i.imgur.com/xONwm7L.gif",
     },
     {
@@ -35,6 +44,9 @@ const pokemons = [
         nome: "Jigglypuff" ,
         descricao: "Jigglypuff has top-notch lung capacity, even by comparison to other Pokémon. It won’t stop singing its lullabies until its foes fall asleep." ,
         tipo: "Fairy",
+        tamanho: "0.5 m",
+        peso: "5.5 kg",
+        genero: "Masculino - Feminino",
         img: "https://i.pinimg.com/originals/e0/db/72/e0db72594143348df0ba94a98212cb13.gif",
     },
     {
@@ -42,6 +54,9 @@ const pokemons = [
         nome: "Diglett" ,
         descricao: "If a Diglett digs through a field, it leaves the soil perfectly tilled and ideal for planting crops." ,
         tipo: "Ground",
+        tamanho: "0.2 m",
+        peso: "0.8 kg",
+        genero: "Masculino - Feminino",
         img: "https://i.pinimg.com/originals/94/6f/61/946f6177a8c56ac8910c3f26e1c729bc.gif",
     },
     {
@@ -49,11 +64,12 @@ const pokemons = [
         nome: "Slowpoke " ,
         descricao: "Slow-witted and oblivious, this Pokémon won’t feel any pain if its tail gets eaten. It won’t notice when its tail grows back, either." ,
         tipo: "Psychic",
+        tamanho: "1.2 m",
+        peso: "36.0 kg",
+        genero: "Masculino - Feminino",
         img: "https://kingdomofzealblog.files.wordpress.com/2017/05/slowpoke-6.gif",
     },
 ]
-
-// let pokemon = undefined;
 
 app.get("/", (req, res) => {
     res.render("index.ejs", {pokemons})
