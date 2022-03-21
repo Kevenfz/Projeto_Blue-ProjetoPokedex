e618de318f6efb8afcd5d5e110d4ad0eabe3977c
kevenfz <keven.ferreira@hotmail.com>
2022-03-21 19:21:11 -0300

    Tudo pronto!

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
 const express = require("express");
 const req = require("express/lib/request");
 const app = express();
-const port = process.env.PORT || 3000;
 const path = require("path");
+const port = 3003;
+
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
